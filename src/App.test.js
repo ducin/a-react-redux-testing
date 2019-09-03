590ae9803062d91c43ff6dea46e3fbf2aaa4e7ab
Tomasz Ducin <tomasz@ducin.it>
2019-09-03 15:14:01 +0200

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
 
-it('should render without crashing', () => {
-  const div = document.createElement('div');
+import App from "./App";
+
+it("should render without crashing #smoke", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('should render without crashing (with prop all=false)', () => {
-  const div = document.createElement('div');
+it("should render without crashing (with prop all=false) #smoke", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App all={false} />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("should match snapshot", () => {
+  const tree = renderer.create(<App />).toJSON();
+
+  expect(tree).toMatchSnapshot();
 });
---
+++ src/components/employees/EmployeeList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export class EmployeeList extends React.Component {
+
+  filtered(){
+    let result = this.props.employees
+
+    if (this.props.filterBy && this.props.filterBy.nationality) {
+      result = result.filter(e => e.nationality === this.props.filterBy.nationality)
+    }
+
+    return result
+  }
+
+  render(){
+    return !this.props.employees
+      ? <h1>No items, baranie!</h1>
+      : <ol>
+        { this.filtered().map(e =>
+          <li key={e.id}>{ e.firstName } {e.lastName}, {e.title}</li>
+        )}
+      </ol>
+  }
+}
