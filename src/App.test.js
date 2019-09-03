93a1431b6ebb6728307627e0f3b28b4eac2249b0
Tomasz Ducin <tomasz@ducin.it>
2019-09-03 11:07:32 +0200

    new test case

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('should render without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should render without crashing (with prop all=false)', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App all={false} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
